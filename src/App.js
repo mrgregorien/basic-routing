5fef370afc06455b1484c57b00e993b8035a9954
mrgregorien <mrwilliamgregorio@gmail.com>
2022-12-26 16:41:09 -0500

    Friend list routing 18v React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import "./App.css"
+
+
+const authUser = [{
+  id: 1001,
+  username: 'Atom',
+  password: Math.random() + 'GzjkbgUYZY' + Math.random() * 50,
+  email: 'atom@gmail.com',
+}];
+
+const friendDB = [{
+  id: 0,
+  name: 'John',
+  email: 'john@example.com',
+  status: 'Last active 2 hrs ago'
+}, {
+  id: 1,
+  name: 'Alex',
+  email: 'Alex@example.com',
+  status: 'Online'
+
+}, {
+  id: 2,
+  name: 'Bob',
+  email: 'bob@example.com',
+  status: 'Offline'
+}
+];
+
+function Feed(props) {
+  return <h1>Welcome {props.username}</h1>
+}
+
+
+function Friends(props) {
+  const friendsCards = friendDB.map(friend => {
+    return <div className='friend-card' key={friend.id}>
+      <div className='friend-details'>
+        <h2>{friend.name}</h2>
+        <p>Contact Email: {friend.email}</p>
+      </div>
+    </div>
+  })
+  return (
+    <>
+      <h1>All friends ({friendDB.length})</h1>
+      {friendsCards}
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Link to="/">Feed</Link>
+        <Link to="/friends">Friends</Link>
+        <Routes>
+          <Route exact path="/" element={<Feed username={authUser[0].username} />} />
+          <Route exact path="/friends" element={<Friends />} />
+        </Routes>
+      </Router>
+    </div >
   );
 }
 
